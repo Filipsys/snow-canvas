c835812ff0d8b6f5f5453dfa362c9bb8c4569f2b
Filipsys <filipsysak@gmail.com>
2024-12-22 22:16:35 +0100

    Added a few comments

--- canvas.js
+++ canvas.js
@@ -1,52 +1,81 @@
 const canvasWidth = 600;
 const canvasHeight = 600;
-let particles = [];
-let numberOfTotalParticlesLogged = 0;
 const timeBetweenParticles = 100;
+let numberOfTotalParticlesLogged = 0;
 let currentTimeBetween = 0;
+let particles = [];
+
+const generateNewParticle = () => {
+  numberOfTotalParticlesLogged += 1; // Increase the total particles (using it as the id)
+
+  // Generate a bunch of random data, duh
+  const randomX = 600 * Math.random();
+  const randomY = 600 * Math.random();
+  const randomSize = 5 * Math.random() + 0.01;
+  const randomLifetime = 3000 * Math.random() + 2000;
+  const randomSpeed = 4 * Math.random() + 1;
+  const randomSway = 4 * Math.random() + 1;
+  const swayDirectionAndSway = -randomSway ? Math.random() >= 0.5 : randomSway;
+
+  particles.push(
+    new Particle(
+      numberOfTotalParticlesLogged,
+      randomX,
+      randomY,
+      randomSize,
+      0,
+      randomLifetime,
+      Date.now(),
+      randomSpeed,
+      swayDirectionAndSway
+    )
+  );
+};
+
+class Particle {
+  constructor(id, x, y, size, opacity, lifetime, appearDate, speed, sway) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.opacity = opacity;
+    this.speed = speed;
+    this.sway = sway;
+    this.lifetime = lifetime;
+    this.appearDate = appearDate;
+  }
+
+  fall() {
+    this.x += this.sway;
+    this.y += this.speed;
+
+    // Check if the particle is out of boundaries, if so, remove it
+    if (this.x + this.sway < 0 || this.x + this.sway > canvasWidth || this.y + this.speed > canvasHeight) {
+      particles.splice(particles.indexOf(this), 1);
+      return;
+    }
+
+    // Draw out the particle & translate it in the direction it's heading in
+    context.fillStyle = `rgb(0 0 0 / ${this.opacity * 100}%)`;
+    context.beginPath();
+    context.arc(this.x, this.y, this.size, 0, Math.PI * 2, true);
+    context.fill();
+
+    context.translate(this.sway, this.speed);
+  }
+}
 
 const initialize = () => {
   const canvasElement = document.getElementById("root");
   if (!canvasElement) {
-    console.error("Canvas element not found");
+    console.error("Canvas element is not found");
     return;
   }
 
   const context = canvasElement.getContext("2d");
   if (!context) {
-    console.error("Canvas context not available");
+    console.error("Canvas context is not available");
     return;
-  }
-
-  class Particle {
-    constructor(id, x, y, size, opacity, lifetime, appearDate, speed, sway) {
-      this.id = id;
-      this.x = x;
-      this.y = y;
-      this.size = size;
-      this.opacity = opacity;
-      this.speed = speed;
-      this.sway = sway;
-      this.lifetime = lifetime;
-      this.appearDate = appearDate;
-    }
-
-    fall() {
-      this.x += this.sway;
-      this.y += this.speed;
-
-      if (this.x + this.sway < 0 || this.x + this.sway > canvasWidth || this.y + this.speed > canvasHeight) {
-        particles.splice(particles.indexOf(this), 1);
-        return;
-      }
-
-      context.fillStyle = `rgb(0 0 0 / ${this.opacity * 100}%)`;
-      context.beginPath();
-      context.arc(this.x, this.y, this.size, 0, Math.PI * 2, true);
-      context.fill();
-
-      context.translate(this.sway, this.speed);
-    }
   }
 
   // particles.push(new Particle(1, 60, 60, 5, 1, 1000, Date.now(), 1, 0.1));
@@ -54,42 +83,18 @@ const initialize = () => {
   // particles.push(new Particle(3, 180, 60, 3, 1, 5000, Date.now(), 1, 0.2));
   // particles.push(new Particle(4, 240, 60, 2, 1, 8000, Date.now(), 2, -0.3));
 
-  const generateNewParticle = () => {
-    numberOfTotalParticlesLogged += 1;
-
-    const randomX = 600 * Math.random();
-    const randomY = 600 * Math.random();
-    const randomSize = 5 * Math.random() + 0.01;
-    const randomLifetime = 3000 * Math.random() + 2000;
-    const randomSpeed = 4 * Math.random() + 1;
-    const randomSway = 4 * Math.random() + 1;
-    const swayDirectionAndSway = -randomSway ? Math.random() >= 0.5 : randomSway;
-
-    particles.push(
-      new Particle(
-        numberOfTotalParticlesLogged,
-        randomX,
-        randomY,
-        randomSize,
-        0,
-        randomLifetime,
-        Date.now(),
-        randomSpeed,
-        swayDirectionAndSway
-      )
-    );
-  };
-
   const updateLoop = () => {
     if (!particles.length) return;
     const currentDate = Date.now();
 
     particles.forEach((particle) => {
+      // If the particle's opacity is below zero, remove it as it is unnecessary
       if (particle.opacity < 0) {
         particles.splice(particles.indexOf(particle), 1);
         return;
       }
 
+      // Check if the particle is new, if so, increase opacity, else decrease it
       if (particle.appearDate + particle.lifetime - currentDate > 300 && particle.opacity < 1) {
         particle.opacity += 0.01;
       } else if (particle.appearDate + particle.lifetime < currentDate) {
@@ -106,6 +111,7 @@ const initialize = () => {
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     context.save();
 
+    // Check if a new particle should be made
     if (currentTimeBetween < timeBetweenParticles) {
       currentTimeBetween += 10;
     } else {
