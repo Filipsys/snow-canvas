516e617738af22dcf16c9b77dadca219d3a32881
Filipsys <filipsysak@gmail.com>
2024-12-24 13:52:18 +0100

    Slight code rework w/ Freiherr

--- canvas.js
+++ canvas.js
@@ -1,106 +1,78 @@
-const canvasWidth = 600;
-const canvasHeight = 600;
+const canvasElement = document.getElementById("root");
+const context = canvasElement.getContext("2d");
+
+if (!canvasElement || !context) throw new Error("Canvas-related issue found");
+
+const canvasWidth = context.canvas.width;
+const canvasHeight = context.canvas.height;
 const timeBetweenParticles = 100;
 let numberOfTotalParticlesLogged = 0;
 let currentTimeBetween = 0;
 let particles = [];
 
 const generateNewParticle = () => {
+  const coinToss = Math.random();
   numberOfTotalParticlesLogged += 1; // Increase the total particles (using it as the id)
-
-  // Generate a bunch of random data, duh
-  const randomX = 600 * Math.random();
-  const randomY = 600 * Math.random();
-  const randomSize = 5 * Math.random() + 0.01;
-  const randomLifetime = 3000 * Math.random() + 2000;
-  const randomSpeed = 4 * Math.random() + 1;
-  const randomSway = 4 * Math.random() + 1;
-  // const swayDirectionAndSway = -randomSway ? Math.random() >= 0.5 : randomSway;
 
   particles.push({
     id: numberOfTotalParticlesLogged,
-    x: randomX,
-    y: randomY,
-    size: randomSize,
+    x: coinToss >= 0.5 ? canvasWidth * Math.random() : 0,
+    y: coinToss < 0.5 ? canvasHeight * Math.random() : 0,
+    size: 3 * Math.random() + 0.01,
     opacity: 0,
-    lifetime: randomLifetime,
-    speed: randomSpeed,
-    sway: randomSway,
+    lifetime: 2000 * Math.random() + 1000,
+    speed: 2 * Math.random() + 1,
+    sway: 1 * Math.random() + 1,
     appearDate: Date.now(),
   });
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  const canvasElement = document.getElementById("root");
-  if (!canvasElement) {
-    console.error("Canvas element is not found");
-    return;
+const updateLoop = () => {
+  // Check if a new particle should be made
+  if (currentTimeBetween < timeBetweenParticles) {
+    currentTimeBetween += 10;
+  } else {
+    currentTimeBetween = 0;
+    generateNewParticle();
   }
 
-  const context = canvasElement.getContext("2d");
-  if (!context) {
-    console.error("Canvas context is not available");
-    return;
-  }
+  if (!particles.length) return;
+  const currentDate = Date.now();
 
-  const updateLoop = () => {
-    if (!particles.length) return;
-    const currentDate = Date.now();
+  const isOutOfBounds = (particle) =>
+    particle.x + particle.sway < 0 ||
+    particle.y + particle.sway < 0 ||
+    particle.x + particle.sway > canvasWidth ||
+    particle.y + particle.speed > canvasHeight;
 
-    particles.forEach((particle) => {
-      // If the particle's opacity is below zero, remove it as it is unnecessary
-      if (particle.opacity < 0) {
-        particles.splice(particles.indexOf(particle), 1);
-        return;
-      }
-
-      // Check if the particle is new, if so, increase opacity, else decrease it
-      if (particle.appearDate + particle.lifetime - currentDate > 300 && particle.opacity < 1) {
-        particle.opacity += 0.01;
-      } else if (particle.appearDate + particle.lifetime < currentDate) {
-        particle.opacity -= 0.01;
-      }
-
-      // Change new particle position
-      particle.x += particle.sway;
-      particle.y += particle.speed;
-
-      // Check if the particle is out of boundaries, if so, remove it
-      if (
-        particle.x + particle.sway < 0 ||
-        particle.x + particle.sway > canvasWidth ||
-        particle.y + particle.speed > canvasHeight
-      ) {
-        particles.splice(particles.indexOf(particle), 1);
-        return;
-      }
-    });
-  };
-
-  const animationLoop = () => {
-    requestAnimationFrame(animationLoop);
-
-    context.clearRect(0, 0, canvasWidth, canvasHeight);
-
-    // Check if a new particle should be made
-    if (currentTimeBetween < timeBetweenParticles) {
-      currentTimeBetween += 10;
-    } else {
-      currentTimeBetween = 0;
-
-      generateNewParticle();
+  particles = particles.filter((particle) => {
+    if (particle.appearDate + particle.lifetime - currentDate > 300 && particle.opacity < 1) {
+      particle.opacity += 0.005;
     }
 
-    particles.forEach((particle) => {
-      // Draw out the particle & translate it in the direction it's heading in
-      context.fillStyle = `rgb(0 0 0 / ${particle.opacity * 100}%)`;
-      context.beginPath();
-      context.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2, true);
-      context.fill();
-    });
+    // Change new particle position
+    particle.x += particle.sway;
+    particle.y += particle.speed;
 
-    updateLoop();
-  };
+    return particle.opacity > 0 && !isOutOfBounds(particle);
+  });
+};
 
-  window.requestAnimationFrame(animationLoop);
-});
+const drawLoop = () => {
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  particles.forEach((particle) => {
+    context.fillStyle = `rgba(0, 0, 0, ${particle.opacity})`;
+    context.beginPath();
+    context.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2, true);
+    context.fill();
+  });
+};
+
+const animationLoop = () => {
+  updateLoop();
+  drawLoop();
+  requestAnimationFrame(animationLoop);
+};
+
+window.requestAnimationFrame(animationLoop);
